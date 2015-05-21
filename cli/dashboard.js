0c809f17bdd5729aa375e58d56e0bd7c3b18b9e6
John Otander <johnotander@gmail.com>
2015-05-21 16:22:42 -0700

    Place stats widgets in a grid

--- cli/dashboard.js
+++ cli/dashboard.js
@@ -6,13 +6,107 @@ var bgColorTable = require('./bg-colors');
 
 module.exports = function(data, options) {
   var screen = blessed.screen();
+  var grid = new contrib.grid({ rows: 3, cols: 3, screen: screen });
 
   screen.key(['escape', 'q', 'C-c'], function(ch, key) {
     return process.exit(0);
   });
 
-  screen.append(propTable(data));
-  screen.append(colorTable(data));
-  screen.append(bgColorTable(data));
+  var propData = data.aggregates.properties.map(function(property) {
+    return [property, data.aggregates[property].total, data.aggregates[property].unique];
+  });
+
+  var fontColorData = data.declarations.unique.color.map(function(decl) {
+    return [decl.value];
+  });
+
+  var bgColorData = data.declarations.unique.backgroundColor.map(function(decl) {
+    return [decl.value];
+  });
+
+  var specificityData = data.selectors.map(function(selector) {
+    return selector.specificity_10;
+  });
+
+  var propertiesTable = grid.set(0, 0, 1, 1, contrib.table, {
+    keys: true,
+    fg: 'white',
+    selectedFg: 'white',
+    selectedBg: 'blue',
+    interactive: true,
+    label: 'Properties - ' + propData.length,
+    border: { type: 'line', fg: 'cyan' },
+    columnSpacing: 10,
+    columnWidth: [16, 6, 6]
+  });
+
+  propertiesTable.setData({
+    headers: ['property', 'total', 'unique'],
+    data: propData
+  });
+
+  var fontColorTable = grid.set(0, 1, 1, 1, contrib.table, {
+    keys: true,
+    fg: 'white',
+    selectedFg: 'white',
+    selectedBg: 'blue',
+    interactive: true,
+    label: 'Font Colors - ' + fontColorData.length,
+    border: { type: 'line', fg: 'cyan' },
+    columnSpacing: 10,
+    columnWidth: [16, 6, 6]
+  });
+
+  fontColorTable.setData({
+    headers: [''],
+    data: fontColorData
+  });
+
+  var bgColorTable = grid.set(0, 2, 1, 1, contrib.table, {
+    keys: true,
+    fg: 'white',
+    selectedFg: 'white',
+    selectedBg: 'blue',
+    interactive: true,
+    label: 'Background Colors - ' + bgColorData.length,
+    border: { type: 'line', fg: 'cyan' },
+    columnSpacing: 10,
+    columnWidth: [16, 6, 6]
+  });
+
+  bgColorTable.setData({
+    headers: [''],
+    data: bgColorData
+  });
+
+  var specificityLineChart = grid.set(1, 0, 1, 3, contrib.line, {
+    style: {
+      line: "yellow",
+      text: "green",
+      baseline: "blue"
+    },
+    xLabelPadding: 1,
+    xPadding: 1,
+    label: 'Specificity - ' + specificityData.length
+  });
+
+  specificityLineChart.setData([{ 
+    x: ['start', 'end'],
+    y: specificityData
+  }]);
+
+  var specificitySparkLine = grid.set(2, 0, 1, 3, contrib.sparkline, {
+    label: '',
+    tags: true,
+    style: {
+      fg: 'blue'
+    }
+  });
+
+  specificitySparkLine.setData(
+    ['Specificity'], 
+    [specificityData]
+  );
+
   screen.render()
 }
